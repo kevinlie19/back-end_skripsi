fc353b65070ed70bb723fad1117e24ed5193d419
Malvin Hariyanto <malvinhariyanto50@gmail.com>
2020-02-12 10:45:35 +0700

    Update many resolvers

--- src/resolvers/userResolvers.ts
+++ src/resolvers/userResolvers.ts
@@ -1,4 +1,12 @@
-import { queryField, mutationField, arg, stringArg, intArg } from 'nexus';
+import {
+  queryField,
+  mutationField,
+  arg,
+  stringArg,
+  intArg,
+  idArg,
+} from 'nexus';
+import { sha256 } from 'js-sha256';
 
 import { Context } from '../main';
 
@@ -23,7 +31,11 @@ export let updateProfile = mutationField('updateProfile', {
     highestScore: intArg(),
     point: intArg(),
   },
-  resolve: async (_, { avatarId, ...updateProfileData }, ctx: Context) => {
+  resolve: async (
+    _,
+    { avatarId, password, ...updateProfileData },
+    ctx: Context,
+  ) => {
     if (avatarId) {
       let avatarCollection = await ctx.prisma
         .user({ id: ctx.userId })
@@ -46,6 +58,9 @@ export let updateProfile = mutationField('updateProfile', {
       }
       throw new Error('Cannot change avatar which is not in avatar collection');
     }
+    if (password) {
+      password = sha256(password + process.env.SALT || '');
+    }
     return await ctx.prisma.updateUser({
       data: {
         ...updateProfileData,
@@ -54,6 +69,20 @@ export let updateProfile = mutationField('updateProfile', {
         id: ctx.userId,
       },
     });
+  },
+});
+
+export let otherProfile = queryField('otherProfile', {
+  type: 'User',
+  args: {
+    id: idArg({ required: true }),
+  },
+  resolve: async (_, { id }, ctx: Context) => {
+    let user = await ctx.prisma.user({ id });
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return user;
   },
 });
 
@@ -85,5 +114,24 @@ export let leaderboard = queryField('leaderboard', {
     return (await ctx.prisma.users()).sort(
       (a, b) => b.highestScore - a.highestScore,
     );
+  },
+});
+
+export let checkPassword = queryField('checkPassword', {
+  type: 'Boolean',
+  args: {
+    password: stringArg({ required: true }),
+  },
+  resolve: async (_, { password }, ctx: Context) => {
+    let user = await ctx.prisma.user({
+      id: ctx.userId,
+    });
+    let hashedPassword = sha256(password + process.env.SALT || '');
+
+    if (user?.password === hashedPassword) {
+      return true;
+    } else {
+      return false;
+    }
   },
 });
